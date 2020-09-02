f4c6deea04b8f3a0dcc5c27bc2895d82c1412973
CircleCI <rob+platform-ui-build@hmn.md>
2020-09-02 17:47:31 +0000

    Build changes from 1723c722f3c30746d5155b959faa844112abd20d
    https://circleci.com/gh/humanmade/experiments/28

---
+++ inc/features/blocks/personalization/edit.js
@@ -0,0 +1,147 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import VariantTitle from './components/variant-title';
+import VariantPanel from './components/variant-panel';
+import VariantToolbar from './components/variant-toolbar';
+
+import withData from './data/edit';
+
+const {
+	BlockControls,
+	InnerBlocks,
+	InspectorControls,
+} = wp.blockEditor;
+const {
+	Button,
+	Toolbar,
+} = wp.components;
+const { __ } = wp.i18n;
+
+/**
+ * Only variants can be direct descendents so that we can generate
+ * usable markup.
+ */
+const ALLOWED_BLOCKS = [ 'altis/personalization-variant' ];
+
+// Audience picker input.
+const Edit = ( {
+	attributes,
+	className,
+	clientId,
+	isSelected,
+	onAddVariant,
+	onCopyVariant,
+	onRemoveVariant,
+	setAttributes,
+	variants,
+} ) => {
+	// Set the block clientId if none currently set.
+	useEffect( () => {
+		if ( ! attributes.clientId ) {
+			setAttributes( { clientId: uuid() } );
+		}
+	}, [] );
+
+	// Track currently selected variant.
+	const defaultVariantClientId = ( variants.length > 0 && variants[ 0 ].clientId ) || null;
+	const [ activeVariant, setVariant ] = useState( defaultVariantClientId );
+
+	// Add a default fallback variant if none present.
+	useEffect( () => {
+		// Ensure at least one variant is present as a fallback.
+		// Note TEMPLATE does not seem to have the desired effect every time.
+		if ( variants.length === 0 ) {
+			setVariant( onAddVariant( { fallback: true } ) );
+		}
+	}, [] );
+
+	// Track the active variant index to show in the title.
+	const activeVariantIndex = variants.findIndex( variant => variant.clientId === activeVariant );
+
+	// Controls that appear before the variant selector buttons.
+	const variantsToolbarControls = [
+		{
+			icon: 'plus',
+			title: __( 'Add a variant', 'altis-experiments' ),
+			className: 'altis-add-variant-button',
+			onClick: () => setVariant( onAddVariant() ),
+		},
+	];
+
+	// When a variant is removed select the preceeding one along unless it's the first in the list.
+	const onRemove = () => {
+		if ( activeVariantIndex === 0 ) {
+			setVariant( variants[ activeVariantIndex + 1 ].clientId );
+		} else {
+			setVariant( variants[ activeVariantIndex - 1 ].clientId );
+		}
+		onRemoveVariant( activeVariant );
+	};
+
+	return (
+		<Fragment>
+			<BlockControls>
+				<Toolbar
+					className="altis-variants-toolbar"
+					controls={ variantsToolbarControls }
+				>
+					<div className="altis-variants-toolbar__tabs">
+						{ variants.map( variant => (
+							<Button
+								key={ `variant-select-${ variant.clientId }` }
+								className={ `altis-variant-button components-icon-button has-text ${ activeVariant === variant.clientId && 'is-active' }` }
+								title={ __( 'Select variant', 'altis-experiments' ) }
+								onClick={ () => setVariant( variant.clientId ) }
+							>
+								<VariantTitle variant={ variant } />
+							</Button>
+						) ) }
+					</div>
+				</Toolbar>
+			</BlockControls>
+			<InspectorControls>
+				{ variants.map( variant => (
+					<VariantPanel
+						key={ `variant-settings-${ variant.clientId }` }
+						variant={ variant }
+					/>
+				) ) }
+			</InspectorControls>
+			<style dangerouslySetInnerHTML={ {
+				__html: `
+					[data-block="${ clientId }"] [data-type="altis/personalization-variant"] {
+						display: none;
+					}
+					[data-block="${ clientId }"] [data-type="altis/personalization-variant"][data-block="${ activeVariant }"] {
+						display: block;
+					}
+				`,
+			} } />
+			<div className={ className }>
+				<div className="wp-core-ui altis-experience-block-header">
+					<span className="altis-experience-block-header__title">
+						{ __( 'Personalized Content', 'altis-experiments' ) }
+						{ ' ・ ' }
+						{ variants.length > 0 && (
+							<VariantTitle variant={ variants[ activeVariantIndex ] } />
+						) }
+					</span>
+					{ isSelected && (
+						<VariantToolbar
+							canRemove={ variants.length > 1 }
+							isFallback={ activeVariant && variants[ activeVariantIndex ].attributes.fallback }
+							onCopy={ () => setVariant( onCopyVariant( activeVariant ) ) }
+							onRemove={ onRemove }
+						/>
+					) }
+				</div>
+				<InnerBlocks
+					allowedBlocks={ ALLOWED_BLOCKS }
+					renderAppender={ false }
+				/>
+			</div>
+		</Fragment>
+	);
+};
+
+export default withData( Edit );
---
+++ inc/features/blocks/personalization/register.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Personalization Block Server Side.
+ *
+ * @phpcs:disable HM.Files.NamespaceDirectoryName.NameMismatch
+ * @phpcs:disable HM.Files.FunctionFileName.WrongFile
+ *
+ * @package altis-experiments
+ */
+
+namespace Altis\Experiments\Features\Blocks\Personalization;
+
+use Altis\Experiments;
+use Altis\Experiments\Features\Blocks;
+use Altis\Experiments\Utils;
+
+const BLOCK = 'personalization';
+
+/**
+ * Registers the block type assets and server side rendering.
+ */
+function setup() {
+	$block_data = Blocks\get_block_settings( BLOCK );
+
+	// Queue up JS files.
+	add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\enqueue_assets' );
+
+	// Register the block.
+	register_block_type( $block_data['name'], [
+		'attributes' => $block_data['settings']['attributes'],
+		'render_callback' => __NAMESPACE__ . '\\render_block',
+	] );
+}
+
+/**
+ * Enqueues the block assets.
+ */
+function enqueue_assets() {
+	wp_enqueue_script(
+		'altis-experiments-features-blocks-personalization',
+		Utils\get_asset_url( 'features/blocks/personalization.js' ),
+		[
+			'wp-plugins',
+			'wp-blocks',
+			'wp-i18n',
+			'wp-editor',
+			'wp-components',
+			'wp-core-data',
+			'wp-edit-post',
+			'altis-experiments-features-blocks-personalization-variant',
+			// Exports the audience UI modal component.
+			'altis-analytics-audience-ui',
+		],
+		null
+	);
+
+	wp_add_inline_script(
+		'altis-experiments-features-blocks-experience',
+		sprintf(
+			'window.Altis = window.Altis || {};' .
+			'window.Altis.Analytics = window.Altis.Analytics || {};' .
+			'window.Altis.Analytics.Experiments = window.Altis.Analytics.Experiments || {};' .
+			'window.Altis.Analytics.Experiments.BuildURL = %s;',
+			wp_json_encode( plugins_url( 'build', Experiments\ROOT_FILE ) )
+		),
+		'before'
+	);
+
+	// Queue up editor CSS.
+	wp_enqueue_style(
+		'altis-experiments-features-blocks-personalization',
+		plugins_url( 'inc/features/blocks/personalization/edit.css', Experiments\ROOT_FILE ),
+		[],
+		'2020-06-03-01'
+	);
+}
+
+/**
+ * Render callback for the experience block.
+ *
+ * Because this block only saves <InnerBlocks.Content> on the JS side,
+ * the content string represents only the wrapped inner block markup.
+ *
+ * @param array $attributes The block's attributes object.
+ * @param string $inner_content The block's saved content.
+ * @return string The final rendered block markup, as an HTML string.
+ */
+function render_block( array $attributes, ?string $inner_content = '' ) : string {
+	$client_id = $attributes['clientId'] ?? null;
+	$class_name = $attributes['className'] ?? '';
+	$align = $attributes['align'] ?? 'none';
+
+	// Warn if a client ID is not set.
+	if ( ! $client_id ) {
+		trigger_error( 'Personalized content blocks must have a client ID set to track impressions. Please re-save the post content to generate one.', E_USER_WARNING );
+		$client_id = wp_generate_uuid4();
+	}
+
+	// Add alignment class.
+	if ( ! empty( $align ) ) {
+		$class_name .= sprintf( 'align%s', $align );
+	}
+
+	// Get a unique ID for the block and apply it to the templates in $inner_content.
+	$inner_content = str_replace( '__PARENT_CLIENT_ID__', $client_id, $inner_content );
+
+	return sprintf(
+		'%s<personalization-block class="%s" client-id="%s"></personalization-block>',
+		$inner_content,
+		$class_name,
+		$client_id
+	);
+}
