891bfefcd3bb2a6639159b2552c80862f11e590b
Robert O'Rourke <robert@o-rourke.org>
2020-11-05 12:22:31 +0000

    Fix linter errors

--- src/experiments.js
+++ src/experiments.js
@@ -7,8 +7,8 @@ function isVisible( element ) {
 	return (
 		rect.top >= 0 &&
 		rect.left >= 0 &&
-		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		rect.bottom <= ( window.innerHeight || document.documentElement.clientHeight ) &&
+		rect.right <= ( window.innerWidth || document.documentElement.clientWidth )
 	);
 }
 
