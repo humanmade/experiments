03437b42345df0a6c2f6240ae4ac274fb65b4565
CircleCI <rob+platform-ui-build@hmn.md>
2021-01-19 15:26:33 +0000

    Build changes from 5ba3c4b11f208164d2e17f5013f2d9ddc06208ba
    https://circleci.com/gh/humanmade/experiments/40

--- inc/features/blocks/personalization/register.php
+++ inc/features/blocks/personalization/register.php
@@ -73,7 +73,7 @@ function enqueue_assets() {
 		'altis-experiments-features-blocks-personalization',
 		plugins_url( 'inc/features/blocks/personalization/edit.css', Experiments\ROOT_FILE ),
 		[],
-		'2020-06-03-01'
+		'2021-01-19-1'
 	);
 }
 
