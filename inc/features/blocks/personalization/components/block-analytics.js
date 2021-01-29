fceb9bad24791d46122accdf582219cabbaf334e
CircleCI <rob+platform-ui-build@hmn.md>
2021-01-29 17:45:26 +0000

    Build changes from 711ff2f00ddd1f879560aa837405cb4bc638806c
    https://circleci.com/gh/humanmade/experiments/44

---
+++ inc/features/blocks/personalization/components/block-analytics.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Views from './views';
+
+const { useSelect } = wp.data;
+const { __, sprintf } = wp.i18n;
+
+const BlockAnalytics = ( { clientId } ) => {
+	const postId = useSelect( select => {
+		return select( 'core/editor' ).getCurrentPostId();
+	} );
+
+	// No post ID so post isn't published, don't show anything.
+	if ( ! postId ) {
+		return null;
+	}
+
+	// Fetch the stats.
+	const data = useSelect( select => {
+		return select( 'analytics/xbs' ).getViews( clientId, postId );
+	}, [ clientId, postId ] );
+	const isLoading = useSelect( select => {
+		return select( 'analytics/xbs' ).getIsLoading();
+	}, [ data ] );
+
+	const totalLoads = ( data && data.loads ) || 0;
+	const totalViews = ( data && data.views ) || 0;
+
+	return (
+		<div className="altis-experience-block-analytics">
+			<h4>{ __( 'Analytics', 'altis-experiments' ) }</h4>
+			<p>{ __( 'Statistics shown are for the configured data retention period.', 'altis-experiments' ) }</p>
+			<Views
+				conversions={ totalViews }
+				conversionsLabel={ sprintf( __( '%d block views', 'altis-experiments' ), totalViews ) }
+				isLoading={ isLoading }
+				label={ sprintf( __( '%d total page views', 'altis-experiments' ), totalLoads ) }
+				total={ totalLoads }
+			/>
+		</div>
+	);
+};
+
+export default BlockAnalytics;
